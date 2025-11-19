a4b3e30643c03fffea2ddb48971b3e057e406532
harsht1608 <harshnitin9250@gmail.com>
2025-11-19 11:37:25 +0530

    Comment out Maven and Java commands in deliver.sh
    Comment out commands in deliver.sh for debugging.

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
-set -e
+# #!/usr/bin/env bash
+# set -e
 
-echo "Running Maven Install..."
-mvn clean
+# echo "Running Maven Install..."
+# mvn clean
 
-echo "Evaluating project name..."
-NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name)
+# echo "Evaluating project name..."
+# NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name)
 
-echo "Evaluating project version..."
-VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
+# echo "Evaluating project version..."
+# VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
 
-echo "Running Application..."
-java -jar target/${NAME}-${VERSION}.jar
+# echo "Running Application..."
+# java -jar target/${NAME}-${VERSION}.jar
