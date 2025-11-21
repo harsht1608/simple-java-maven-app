38562ea5a5e4e8fe14eef16b088e20599e4b5d67
harsht1608 <harshnitin9250@gmail.com>
2025-11-21 10:51:24 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,16 +4,7 @@ package com.mycompany.app;
  * Hello world!
  */
 public class App {
-
-    private static final String MESSAGE = "Hello World!";
-
-    public App() {}
-
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-
-    public String getMessage() {
-        return MESSAGE;
+        System.out.println("Hello World!");
     }
 }
